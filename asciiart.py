5649cf58eaa0684ad92899daa91b32b45706c319
Pierre <me@lebrunpierre.com>
2022-03-25 07:57:49 -0700

    adding game logic

--- asciiart.py
+++ asciiart.py
@@ -28,4 +28,25 @@ scissors = '''
 rock_paper_scissors = {1: rock, 2: paper, 3: scissors}
 
 
-def game_win_logic (player, computer):
+def is_the_player_the_winner(player, computer):
+    '''
+
+    :param player: user input
+    :param computer: computer input
+    :return: bool
+
+    Evluates if player wins.
+    returns true if thy did false if the did not
+    '''
+
+    if player == 1 and computer == 3:
+        return "Winner"
+
+    elif player == 2 and computer == 1:
+        return "Winner"
+
+    elif player == 3 and computer == 2:
+        return "Winner"
+
+    elif player == computer:
+        return "Draw"
